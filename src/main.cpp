046b0a2e644ab22b08a3b0d2383946ad7de754bd
Marcus Klein <himself@kleini.org>
2019-03-17 19:31:46 +0100

    added relay

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,21 @@
 #define FW_NAME "Shelly 1"
-#define FW_VERSION "0.1.0"
+#define FW_VERSION "0.2.0"
 
 #include "Homie.h"
+#include "RelayNode.hpp"
 
 // high for closing relay
 #define PIN_RELAY 4
 // reads low when SW shortened to AC/L | GND
 #define PIN_SWITCH 5
 
+RelayNode relay("relay", PIN_RELAY);
+
 void setup() {
   Serial.begin(115200);
   Serial << endl << endl;
+
+  pinMode(PIN_RELAY, OUTPUT);
 
   Homie_setFirmware(FW_NAME, FW_VERSION);
   Homie.disableResetTrigger();
